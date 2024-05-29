cbe7762d993e0389c8efc627aaf612096266831c
Krishna <chavank901@gmail.com>
2024-05-29 14:46:16 +0530

    second commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,10 +12,18 @@ const Navbar = () => {
             <img className='mx-2 w-25 h-10' src={logo} alt="logo" />
       </div>
       <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
-        <FaLinkedin />
-        <FaGithub />
-        <FaInstagram />
-        <FaSquareXTwitter />
+        <a className='hover:text-cyan-400 active:text-cyan-600' href='https://www.linkedin.com/in/krishnachavan1/'>
+          <FaLinkedin />
+        </a>
+        <a className='hover:text-cyan-400 active:text-cyan-600' href="https://github.com/chavankrishna">
+          <FaGithub />
+        </a>
+        <a className='hover:text-cyan-400 active:text-cyan-600' href="https://www.instagram.com/_krishna.__21/">
+          <FaInstagram />
+        </a>
+        <a className='hover:text-cyan-400 active:text-cyan-600' href="https://x.com/krishna18915181">
+          <FaSquareXTwitter />
+        </a>
       </div>
     </nav>
   );
